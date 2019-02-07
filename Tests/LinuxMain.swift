c4a5d624bc75228e53232d4939a25e828b4a9a88
Jeff Verkoeyen <jverkoey@gmail.com>
2019-02-06 19:59:53 -0500

    Add linux tests.

--- Tests/LinuxMain.swift
+++ Tests/LinuxMain.swift
@@ -3,6 +3,7 @@ import XCTest
 import LengthEncodedStringTests
 import LengthEncodedIntegerTests
 import MySqlConnectorTests
+import IteratorProtocol_nextTests
 import Data_xoredTests
 import FixedWidthInteger_bytesTests
 
@@ -10,6 +11,7 @@ var tests = [XCTestCaseEntry]()
 tests += LengthEncodedStringTests.__allTests()
 tests += LengthEncodedIntegerTests.__allTests()
 tests += MySqlConnectorTests.__allTests()
+tests += IteratorProtocol_nextTests.__allTests()
 tests += Data_xoredTests.__allTests()
 tests += FixedWidthInteger_bytesTests.__allTests()
 
