9a144a7c7ac65fdaa1603b14c2f6cdbce628007d
Jeff Verkoeyen <jverkoey@gmail.com>
2019-02-11 22:43:59 -0500

    Delete unused code.

--- Tests/LinuxMain.swift
+++ Tests/LinuxMain.swift
@@ -1,18 +1,16 @@
 import XCTest
 
 import MySqlConnectorTests
+import FixedWidthInteger_bytesTests
 import LazyDataStreamTests
 import CustomStringConvertible_descriptionTests
 import Data_xoredTests
-import FixedWidthInteger_bytesTests
-import IteratorProtocol_nextTests
 
 var tests = [XCTestCaseEntry]()
 tests += MySqlConnectorTests.__allTests()
+tests += FixedWidthInteger_bytesTests.__allTests()
 tests += LazyDataStreamTests.__allTests()
 tests += CustomStringConvertible_descriptionTests.__allTests()
 tests += Data_xoredTests.__allTests()
-tests += FixedWidthInteger_bytesTests.__allTests()
-tests += IteratorProtocol_nextTests.__allTests()
 
 XCTMain(tests)
