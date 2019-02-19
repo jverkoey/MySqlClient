da4c605c4d07fdd14521e184755253adce0d80e1
featherless <jverkoey@gmail.com>
2019-02-19 09:26:24 -0500

    Remove unnecessary imports (#64)

--- Tests/MySqlConnectorTests/TableTests.swift
+++ Tests/MySqlConnectorTests/TableTests.swift
@@ -12,8 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-import BinaryCodable
-@testable import MySqlConnector
+import MySqlConnector
 import XCTest
 
 struct TableColumnDescription: Decodable, Equatable {
