e6ae915d5464d96d77bf129b45ec48e164a4f5bd
Jeff Verkoeyen <jverkoey@gmail.com>
2019-02-18 23:51:01 -0500

    Regenerate tests.

--- Tests/LinuxMain.swift
+++ Tests/LinuxMain.swift
@@ -1,12 +1,10 @@
 import XCTest
 
 import MySqlConnectorTests
-import BinaryCodableTests
 import MySqlQueryTests
 
 var tests = [XCTestCaseEntry]()
 tests += MySqlConnectorTests.__allTests()
-tests += BinaryCodableTests.__allTests()
 tests += MySqlQueryTests.__allTests()
 
 XCTMain(tests)
