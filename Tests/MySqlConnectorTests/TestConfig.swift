192acbf8f6446d0d393c7f376188aafe64151456
Jeff Verkoeyen <jverkoey@gmail.com>
2020-07-05 22:52:43 -0400

    More.

--- Tests/MySqlConnectorTests/TestConfig.swift
+++ Tests/MySqlConnectorTests/TestConfig.swift
@@ -50,189 +50,181 @@ let testEnvironments: [SqlServerTestEnvironment] = [
     serverType: .MySql(version: "8.0.20")
   )
 ]
-//
-//struct TestRunner {
-//  struct Environment {
-//    let host: String = "localhost"
-//    let port: Int32 = 3306
-//    let user: String = "root"
-//    let pass: String = ""
-//    let setUp: () -> Void
-//    let tearDown: () -> Void
-//    let serverType: SqlServerType
-//
-//    #if compiler(>=5.3)
-//    func AssertEqual<T>(
-//      _ arg1: T,
-//      _ arg2: [SqlServerType: T],
-//      file: StaticString = #filePath,
-//      line: UInt = #line
-//    ) where T : Equatable {
-//      XCTAssertEqual(arg1, arg2[serverType], file: file, line: line)
-//    }
-//    #else
-//    func AssertEqual<T>(
-//      _ arg1: T,
-//      _ arg2: [SqlServerType: T],
-//      file: StaticString = #file,
-//      line: UInt = #line
-//    ) where T : Equatable {
-//      XCTAssertEqual(arg1, arg2[serverType], file: file, line: line)
-//    }
-//    #endif
-//  }
-//  func test(_ test: (Environment, BufferedData) throws -> Void) throws {
-//    try environments.forEach { environment in
-//      var socket: Socket!
-//      var socketDataStream: BufferedData!
-//      environment.setUp()
-//      defer {
-//        if socket != nil {
-//          socket.close()
-//          socket = nil
-//        }
-//        socketDataStream = nil
-//
-//        environment.tearDown()
-//      }
-//
-//      for backoff in [1, 2, 4, 8] as [UInt32] {
-//        do {
-//          socket = try Socket.create()
-//          try socket.connect(to: environment.host, port: environment.port)
-//        } catch {
-//          sleep(backoff)
-//          continue
-//        }
-//        break
-//      }
-//
-//      if !socket.isConnected {
-//        return
-//      }
-//
-//      var buffer = Data(capacity: socket.readBufferSize)
-//      socketDataStream = BufferedData(reader: AnyBufferedDataSource(read: { recommendedAmount in
-//        if buffer.count == 0 {
-//          _ = try socket.read(into: &buffer)
-//        }
-//        let pulledData = buffer.prefix(recommendedAmount)
-//        buffer = buffer.dropFirst(recommendedAmount)
-//        return pulledData
-//      }, isAtEnd: {
-//        do {
-//          return try buffer.isEmpty && !socket.isReadableOrWritable(waitForever: false, timeout: 0).readable
-//        } catch {
-//          return true
-//        }
-//      }))
-//
-//      try test(environment, socketDataStream)
-//    }
-//  }
-//  let environments: [Environment]
-//  init() {
-//    print("Hello")
-//
-//    let testDirectory = URL(fileURLWithPath: #file).deletingLastPathComponent()
-//    print("Test directory: \(testDirectory)")
-//    let testCacheDirectory = testDirectory.appendingPathComponent(".cache")
-//
-//    let fileManager = FileManager.default
-//    try! fileManager.createDirectory(at: testCacheDirectory, withIntermediateDirectories: true, attributes: nil)
-//
-//    self.environments = testEnvironments.map { environment in
-//      #if os(Linux)
-//      let hostEnvironment: HostEnvironment = .linux
-//      #elseif os(macOS)
-//      let hostEnvironment: HostEnvironment = .macOS
-//      #endif
-//
-//      let environmentUrl = environment.url[hostEnvironment]!
-//
-//      #if os(Linux)
-//      let environmentPath = testCacheDirectory.appendingPathComponent(environmentUrl.deletingPathExtension().lastPathComponent)
-//      #elseif os(macOS)
-//      let environmentPath = testCacheDirectory.appendingPathComponent(environmentUrl.deletingPathExtension().deletingPathExtension().lastPathComponent)
-//      #endif
-//      if !fileManager.fileExists(atPath: environmentPath.path) {
-//        let tarPath = testCacheDirectory.appendingPathComponent(environmentUrl.lastPathComponent)
-//        if !fileManager.fileExists(atPath: tarPath.path) {
-//          print("Downloading \(environment.name) from \(environment.url)...")
-//          let tar = try! Data(contentsOf: environmentUrl)
-//          try! tar.write(to: tarPath)
-//        }
-//        let task = Process()
-//        task.launchPath = "/usr/bin/tar"
-//        task.arguments = [
-//          "-xf",
-//          tarPath.path,
-//          "-C",
-//          testCacheDirectory.path
-//        ]
-//        task.launch()
-//        task.waitUntilExit()
-//
-//        #if os(Linux)
-//        let dataTask = Process()
-//        dataTask.launchPath = "/usr/bin/tar"
-//        dataTask.arguments = [
-//          "-xf",
-//          testCacheDirectory.appendingPathComponent("data.tar.xz").path,
-//          "-C",
-//          testCacheDirectory.path
-//        ]
-//        dataTask.launch()
-//        dataTask.waitUntilExit()
-//        #endif
-//      }
-//
-//      let serverPath = environmentPath.appendingPathComponent(environment.serverPath[hostEnvironment]!)
-//      let dataPath = environmentPath.appendingPathComponent("data")
-//      let initialDataPath = environmentPath.appendingPathComponent("data_initial")
-//      if !fileManager.fileExists(atPath: initialDataPath.path) {
-//        try! fileManager.createDirectory(at: initialDataPath, withIntermediateDirectories: true, attributes: nil)
-//
-//        let initializationTask = Process()
-//        initializationTask.launchPath = serverPath.path
-//        initializationTask.arguments = [
-//          "--basedir=\(environmentPath.path)",
-//          "--datadir=\(initialDataPath.path)",
-//          "--initialize-insecure"  // We don't require a password.
-//        ]
-//        initializationTask.launch()
-//        initializationTask.waitUntilExit()
-//      }
-//
-//      let runTask = Process()
-//      runTask.launchPath = serverPath.path
-//      runTask.arguments = [
-//        "--basedir=\(environmentPath.path)",
-//        "--datadir=\(dataPath.path)",
-//        "--port=3306",
-//      ]
-//
-//      return Environment(
-//        setUp: {
-//          if fileManager.fileExists(atPath: dataPath.path) {
-//            try! fileManager.removeItem(at: dataPath)
-//          }
-//          try! fileManager.copyItem(atPath: initialDataPath.path, toPath: dataPath.path)
-//          runTask.launch()
-//        }, tearDown: {
-//          runTask.terminate()
-//        }, serverType: environment.serverType
-//      )
-//    }
-//  }
-//}
+
+struct TestRunner {
+  struct Environment {
+    let host: String = "localhost"
+    let port: Int32 = 3306
+    let user: String = "root"
+    let pass: String = ""
+    let setUp: () -> Void
+    let tearDown: () -> Void
+    let serverType: SqlServerType
+
+    #if compiler(>=5.3)
+    func AssertEqual<T>(
+      _ arg1: T,
+      _ arg2: [SqlServerType: T],
+      file: StaticString = #filePath,
+      line: UInt = #line
+    ) where T : Equatable {
+      XCTAssertEqual(arg1, arg2[serverType], file: file, line: line)
+    }
+    #else
+    func AssertEqual<T>(
+      _ arg1: T,
+      _ arg2: [SqlServerType: T],
+      file: StaticString = #file,
+      line: UInt = #line
+    ) where T : Equatable {
+      XCTAssertEqual(arg1, arg2[serverType], file: file, line: line)
+    }
+    #endif
+  }
+  func test(_ test: (Environment, BufferedData) throws -> Void) throws {
+    try environments.forEach { environment in
+      var socket: Socket!
+      var socketDataStream: BufferedData!
+      environment.setUp()
+      defer {
+        if socket != nil {
+          socket.close()
+          socket = nil
+        }
+        socketDataStream = nil
+
+        environment.tearDown()
+      }
+
+      for backoff in [1, 2, 4, 8] as [UInt32] {
+        do {
+          socket = try Socket.create()
+          try socket.connect(to: environment.host, port: environment.port)
+        } catch {
+          sleep(backoff)
+          continue
+        }
+        break
+      }
+
+      if !socket.isConnected {
+        return
+      }
+
+      var buffer = Data(capacity: socket.readBufferSize)
+      socketDataStream = BufferedData(reader: AnyBufferedDataSource(read: { recommendedAmount in
+        if buffer.count == 0 {
+          _ = try socket.read(into: &buffer)
+        }
+        let pulledData = buffer.prefix(recommendedAmount)
+        buffer = buffer.dropFirst(recommendedAmount)
+        return pulledData
+      }, isAtEnd: {
+        do {
+          return try buffer.isEmpty && !socket.isReadableOrWritable(waitForever: false, timeout: 0).readable
+        } catch {
+          return true
+        }
+      }))
+
+      try test(environment, socketDataStream)
+    }
+  }
+  let environments: [Environment]
+  init() {
+    print("Hello")
+
+    let testDirectory = URL(fileURLWithPath: #file).deletingLastPathComponent()
+    print("Test directory: \(testDirectory)")
+    let testCacheDirectory = testDirectory.appendingPathComponent(".cache")
+
+    let fileManager = FileManager.default
+    try! fileManager.createDirectory(at: testCacheDirectory, withIntermediateDirectories: true, attributes: nil)
+
+    self.environments = testEnvironments.map { environment in
+      #if os(Linux)
+      let hostEnvironment: HostEnvironment = .linux
+      #elseif os(macOS)
+      let hostEnvironment: HostEnvironment = .macOS
+      #endif
+
+      let environmentUrl = environment.url[hostEnvironment]!
+
+      #if os(Linux)
+      let environmentPath = testCacheDirectory.appendingPathComponent(environmentUrl.deletingPathExtension().lastPathComponent)
+      #elseif os(macOS)
+      let environmentPath = testCacheDirectory.appendingPathComponent(environmentUrl.deletingPathExtension().deletingPathExtension().lastPathComponent)
+      #endif
+      if !fileManager.fileExists(atPath: environmentPath.path) {
+        let tarPath = testCacheDirectory.appendingPathComponent(environmentUrl.lastPathComponent)
+        if !fileManager.fileExists(atPath: tarPath.path) {
+          print("Downloading \(environment.name) from \(environment.url)...")
+          let tar = try! Data(contentsOf: environmentUrl)
+          try! tar.write(to: tarPath)
+        }
+        let task = Process()
+        task.launchPath = "/usr/bin/tar"
+        task.arguments = [
+          "-xf",
+          tarPath.path,
+          "-C",
+          testCacheDirectory.path
+        ]
+        task.launch()
+        task.waitUntilExit()
+
+        #if os(Linux)
+        let dataTask = Process()
+        dataTask.launchPath = "/usr/bin/tar"
+        dataTask.arguments = [
+          "-xf",
+          testCacheDirectory.appendingPathComponent("data.tar.xz").path,
+          "-C",
+          testCacheDirectory.path
+        ]
+        dataTask.launch()
+        dataTask.waitUntilExit()
+        #endif
+      }
+
+      let serverPath = environmentPath.appendingPathComponent(environment.serverPath[hostEnvironment]!)
+      let dataPath = environmentPath.appendingPathComponent("data")
+      let initialDataPath = environmentPath.appendingPathComponent("data_initial")
+      if !fileManager.fileExists(atPath: initialDataPath.path) {
+        try! fileManager.createDirectory(at: initialDataPath, withIntermediateDirectories: true, attributes: nil)
+
+        let initializationTask = Process()
+        initializationTask.launchPath = serverPath.path
+        initializationTask.arguments = [
+          "--basedir=\(environmentPath.path)",
+          "--datadir=\(initialDataPath.path)",
+          "--initialize-insecure"  // We don't require a password.
+        ]
+        initializationTask.launch()
+        initializationTask.waitUntilExit()
+      }
+
+      let runTask = Process()
+      runTask.launchPath = serverPath.path
+      runTask.arguments = [
+        "--basedir=\(environmentPath.path)",
+        "--datadir=\(dataPath.path)",
+        "--port=3306",
+      ]
+
+      return Environment(
+        setUp: {
+          if fileManager.fileExists(atPath: dataPath.path) {
+            try! fileManager.removeItem(at: dataPath)
+          }
+          try! fileManager.copyItem(atPath: initialDataPath.path, toPath: dataPath.path)
+          runTask.launch()
+        }, tearDown: {
+          runTask.terminate()
+        }, serverType: environment.serverType
+      )
+    }
+  }
+}
 //
 //let testRunner = TestRunner()
-//
-//private func getEnvironmentVariable(named name: String) -> String? {
-//  if let environmentValue = getenv(name) {
-//    return String(cString: environmentValue)
-//  } else {
-//    return nil
-//  }
-//}
