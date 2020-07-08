db8ec83221af0ac9c0dbf712e52cb26b3b78c712
Jeff Verkoeyen <jverkoey@gmail.com>
2020-07-08 15:17:38 -0400

    More renames.

--- Package.swift
+++ Package.swift
@@ -1,5 +1,5 @@
 // swift-tools-version:4.2
-// Copyright 2019-present the MySqlConnector authors. All Rights Reserved.
+// Copyright 2019-present the MySqlClient authors. All Rights Reserved.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
